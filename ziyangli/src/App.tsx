74c05f21075a7560d75f9d8303b6caa3114384d2
Ziyang Li <ziyangzero@gmail.com>
2024-03-10 10:11:33 -0500

    start template

---
+++ ziyangli/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <p>my name is ziyang li and I am a software developer.</p>
+    </div>
+  );
+}
+
+export default App;
