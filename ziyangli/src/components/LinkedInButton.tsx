6973d7d2ecea5191490f79fc16a2da0cb209b5b5
Ziyang Li <ziyangzero@gmail.com>
2025-09-10 00:13:00 -0500

    update formatting

--- ziyangli/src/components/LinkedInButton.tsx
+++ ziyangli/src/components/LinkedInButton.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const LinkedInButton: React.FC = () => {
   return (
-    <a href="https://www.linkedin.com/in/ziyangg" target="_blank" rel="noopener noreferrer" className="linkedin-button">
-      <LinkedInIcon sx={{ color: 'white' }} />
+    <a
+      href="https://www.linkedin.com/in/ziyangg"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="linkedin-button"
+    >
+      <LinkedInIcon sx={{ color: "white" }} />
     </a>
   );
 };
